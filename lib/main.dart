938a3fabb78391dd1f1f380e6afe7096232787e2
Chathra Navoda Ranasinghe <91416868+ChathraNavoda@users.noreply.github.com>
2023-03-20 16:21:26 +0530

    configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:boostme/responsive/mobile_screen_layout.dart';
 import 'package:boostme/responsive/responsive_layout_screen.dart';
 import 'package:boostme/responsive/web_screen_layout.dart';
 import 'package:boostme/utils/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
